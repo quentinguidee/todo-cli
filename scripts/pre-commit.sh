af33f066e67a6a23e9a3c8245d828ef7000afa7c
Quentin Guidée <quentin.guidee@gmail.com>
2021-06-15 09:34:31 +0200

    Add pycodestyle to pre-commit hook and hide output

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env sh
 
-printf "Running tests before commiting...\n\n"
+printf "\nTests: Running..."
 
-python3 tests.py
+python3 tests.py >/dev/null 2>&1
 exit_code=$?
 
 if [ $exit_code -ne 0 ]; then
-    echo "Tests failed. Cannot commit."
+    printf "\rTests: FAILED.\n\n"
+    echo "The tests don't pass. Cannot commit."
+    echo "Re-run 'python3 tests.py' for more info."
+    echo ""
     echo "If you want to force commit, you can add the --no-verify flag."
     exit 1
 fi
+
+printf "\rTests: PASSED.\n"
+
+printf "pycodestyle: Running..."
+
+pycodestyle . --max-line-length=120 >/dev/null 2>&1
+exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+    printf "\rpycodestyle: FAILED.\n\n"
+    echo "Some formatting mistakes found. Cannot commit."
+    echo "Re-run 'pycodestyle . --max-line-length=120' for more info."
+    echo ""
+    echo "If you want to force commit, you can add the --no-verify flag."
+    exit 1
+fi
+
+printf "\rpycodestyle: PASSED.\n\n"
